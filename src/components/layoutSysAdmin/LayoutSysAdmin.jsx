a196aee3debcaa95d9f489d6ce013f55c766ac67
DarkusA <ramiperez06austral@gmail.com>
2026-09-03 05:55:10 -0300

    SysAdminLayout

---
+++ src/components/layoutSysAdmin/LayoutSysAdmin.jsx
@@ -0,0 +1,22 @@
+import "./LayoutSysAdmin.css";
+import { Link, Outlet } from "react-router";
+
+function LayoutSysAdmin() {
+    return (
+        <div className="layout">
+            <header className="header">
+                <h1>Sunflower</h1>
+                <nav>
+                    <Link to="/catalog">Catalog</Link>
+                    <span>|</span>
+                    <Link to="/users">Users</Link>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default LayoutSysAdmin;
